e2f87de4a8044d0ae295f491eda59fea9772492c
Justin McLean <justin.t.mclean@gmail.com>
2021-06-13 03:37:13 -0600

    modify call to super constructor and modify constructor

---
+++ src/main/java/me/madcabbage/mpwebsocketserver/Spectator.java
@@ -0,0 +1,11 @@
+package me.madcabbage.mpwebsocketserver;
+
+import org.java_websocket.WebSocket;
+
+public class Spectator extends AbstractViewer {
+
+
+    public Spectator(WebSocket connection) {
+        super(connection);
+    }
+}
